cf950aeb41a4c4cf7d6cdc1251a87a6bd16a21af
Qi Yang <nayamama@hotmail.com>
2018-10-26 18:03:27 -0400

    remove APP_SETTING, use .ini to set ENV

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
+import os
 from flask import Flask
+import configparser
 
 app = Flask(__name__)
+config = configparser.ConfigParser()
+config.read(os.path.abspath(os.path.join(".ini")))
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
 
 @app.route('/<name>')
 def hello_name(name):
